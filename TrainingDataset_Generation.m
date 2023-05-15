d4ed16367183c315cb42c1cb711bb18b5fdfc185
Zhi-Huo XU <Z-H-XU@users.noreply.github.com>
2023-05-15 12:05:39 +0800

    V1.0

--- TrainingDataset_Generation.m
+++ TrainingDataset_Generation.m
@@ -6,7 +6,7 @@
 %
 % --------------------------------------------------------------------------------------------
 %
-% authors:           Zhihuo Xu, Yyuexia Wang, Linyi Zhou,  et al.
+% authors:           Zhihuo Xu, Yuexia Wang, Linyi Zhou,  et al.
 %
 % web page:       https://github.com/Z-H-XU/Benchmark-Visibility
 %
@@ -19,7 +19,7 @@
 % This work should be used for nonprofit purposes only.
 % --------------------------------------------------------------------------------------------
 % If you use the code, please kindly cite the following paper:  
-% Zhihuo Xu, Yyuexia Wang, Linyi Zhou,  et al.,  First Visibility Sensing Benchmark for Intelligent Transportation Systems 
+% Zhihuo Xu, Yuexia Wang, Linyi Zhou,  et al.,  First Visibility Sensing Benchmark for Intelligent Transportation Systems 
 % using Traffic Video Networks: Dataset and Methodology
 % IEEE Transactions on Intelligent Transportation Systems, Under Review.
 % Thank you!
