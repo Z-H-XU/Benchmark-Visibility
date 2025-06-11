39186ddb78bdeba91b99af2f6c1447595e535523
Zhi-Huo XU <Z-H-XU@users.noreply.github.com>
2025-06-11 10:15:59 +0800

    V 1.3
    Update citation.

--- SVD_LSTM_ConvLSTM.m
+++ SVD_LSTM_ConvLSTM.m
@@ -18,10 +18,10 @@
 % All rights reserved.
 % This work should be used for nonprofit purposes only.
 % --------------------------------------------------------------------------------------------
-%If you use the dataset or the code, please kindly cite our paper:
-%Yuexia Wang, Linyi Zhou, and Zhihuo Xu. "Traffic Road Visibility Retrieval in the Internet of Video Things through Physical Feature Based Learning Network."
-%IEEE Transactions on Intelligent Transportation Systems,  vol. 26, no. 3, pp. 3629-3642, March 2025, doi: 10.1109/TITS.2024.3519137, https://ieeexplore.ieee.org/document/10817150. 
-%Thank you!
+% If you use the dataset or the code, please kindly cite our paper:
+% Yuexia Wang, Linyi Zhou, and Zhihuo Xu. "Traffic Road Visibility Retrieval in the Internet of Video Things through Physical Feature Based Learning Network."
+% IEEE Transactions on Intelligent Transportation Systems,  vol. 26, no. 3, pp. 3629-3642, March 2025, doi: 10.1109/TITS.2024.3519137, https://ieeexplore.ieee.org/document/10817150. 
+% Thank you!
 
 clc;clear;close all;
 
